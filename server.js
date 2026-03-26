0c57d61b444df8e9e7ba321f407f5cbb3c097e30
afiyasultana808-coder <afiyasultana808@gmail.com>
2026-03-26 11:48:17 +0530

    Update database connection environment variables

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 // DB connection
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 // Test route
