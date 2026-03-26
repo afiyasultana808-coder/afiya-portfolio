79b0f2d0b4a81ab66a003f6632a212d184791723
afiyasultana808-coder <afiyasultana808@gmail.com>
2026-03-26 11:50:17 +0530

    Add MySQL port to database connection
    Added port configuration for MySQL connection.

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQLPORT,
 });
 
 // Test route
